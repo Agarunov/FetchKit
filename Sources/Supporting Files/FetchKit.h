f33acaf3654a910272a34f023c7b9dfba717df6b
Anton Agarunov <agarunov.anton@gmail.com>
2017-04-22 20:13:25 +0300

    init project

---
+++ Sources/Supporting Files/FetchKit.h
@@ -0,0 +1,15 @@
+//
+//  FetchKit iOS.h
+//  FetchKit iOS
+//
+//  Created by Anton Agarunov on 22.04.17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FetchKit.
+FOUNDATION_EXPORT double FetchKitVersionNumber;
+
+//! Project version string for FetchKit.
+FOUNDATION_EXPORT const unsigned char FetchKitVersionString[];
