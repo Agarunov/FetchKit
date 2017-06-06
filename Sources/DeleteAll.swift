01d281625ca17462459b42f8a038297e9bfac502
Anton Agarunov <agarunov.anton@gmail.com>
2017-06-06 12:39:12 +0300

    update deleteAll

--- Sources/DeleteAll.swift
+++ Sources/DeleteAll.swift
@@ -18,18 +18,32 @@ open class deleteAll<ModelType: NSManagedObject>: FetchRequest<ModelType> {
     ///
     /// - returns: Deleted `ModelType` objects count
     ///
-    /// - throws: Core Data error if fetch fails
+    /// - throws: Core Data error if fetch fails    
     @discardableResult
     open func execute(in context: NSManagedObjectContext) throws -> Int {
-        let request: NSFetchRequest<ModelType> = buildFetchRequest()
-        request.returnsObjectsAsFaults = true
-        request.includesPropertyValues = false
-        
-        let result = try context.fetch(request)
-        for obj in result {
-            context.delete(obj)
+        if #available(iOS 9.0, tvOS 9.0, watchOS 2.0, macOS 10.11, *) {
+            let request: NSFetchRequest<NSFetchRequestResult> = buildFetchRequest()
+            let batchRequest = NSBatchDeleteRequest(fetchRequest: request)
+            batchRequest.resultType = .resultTypeCount
+            
+            guard let batchResult = try context.execute(batchRequest) as? NSBatchDeleteResult,
+                let count = batchResult.result as? Int else {
+                    fatalError("Invalid context execution result type")
+            }
+            
+            return count
+        } else {
+            let request: NSFetchRequest<ModelType> = buildFetchRequest()
+            request.returnsObjectsAsFaults = true
+            request.includesPropertyValues = false
+            
+            let result = try context.fetch(request)
+            for obj in result {
+                context.delete(obj)
+            }
+            
+            return result.count
         }
-        return result.count
     }
     
 }
